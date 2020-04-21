b51896bca66524b34663b2ca307b26d3ff22da26
hamdaoui hamza <hhamdaoui31@gmail.com>
2020-04-21 17:20:15 +0000

    Hot fix

--- app/Http/Resources/FactureResource.php
+++ app/Http/Resources/FactureResource.php
@@ -14,14 +14,24 @@ class FactureResource extends JsonResource
      */
     public function toArray($request)
     {
+        $htArray = [];
+        $ttcArray = [];
+        foreach ($this->articles as $article) {
+            array_push($htArray, $article->total_ht);
+
+            array_push($ttcArray, $article->total_ttc);
+        }
+
         return [
             "Facture_id" => $this->id,
             "User_id" =>  $this->user_id,
             "Statut_id" => $this->status_id,
-
+            "Total_ht" => array_sum($htArray),
+            "Total_ttc" => array_sum($ttcArray),
             "Reglement" => new ReglementResource($this->reglements),
             "Artiles" => ArticleResource::collection($this->articles),
             "Text_Document" => new TextDocumentResource($this->textDocument)
+
         ];
     }
 }
